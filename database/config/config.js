5560926dfea65e9d01d0fcfeafde5b0d856850e6
Jfisher37 <joseph.eric.fisher@gmail.com>
2023-09-28 15:16:02 +0100

    added dialect explicitly in config.

--- database/config/config.js
+++ database/config/config.js
@@ -5,7 +5,8 @@ const connectionString = process.env.DATABASE_URL;
 
 const client = new Client({
   connectionString: connectionString,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  dialect: 'postgres'
 });
 
 module.exports = client;
