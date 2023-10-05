1ddc5cf0e458a7ed6bfb4a65d701d63cf8a7e9bd
Jfisher37 <joseph.eric.fisher@gmail.com>
2023-10-05 21:12:32 +0100

    added boilerplate code into config.js.

--- database/models/index.js
+++ database/models/index.js
@@ -10,6 +10,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+console.log('Config', config);
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
