85396443fcaac7ac533c453594467d661661472d
Jfisher37 <joseph.eric.fisher@gmail.com>
2024-01-02 15:32:00 -0500

    for production, have sequelize instantiating just from database url.

--- database/config/config.js
+++ database/config/config.js
@@ -1,31 +1,3 @@
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-client.dialect = 'postgres';
-client.dialectOptions = {
-  ssl: { 
-    require: true,
-    rejectUnauthorized: false },
-}
-
-
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
@@ -51,5 +23,16 @@ module.exports = {
         rejectUnauthorized: false },
     }
   },
-  production: client
+  production: {
+    url: process.env.DATABASE_URL,
+    database: process.env.DATABASE,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { 
+        require: true,
+        rejectUnauthorized: false },
+    }
+  },
 }
